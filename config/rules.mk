66be77164022c1f1472b7b92ad101d56011d4697
ChairBorn <mysakyuriy@gmail.com>
2024-10-28 17:47:32 -0700

    add rules file
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

---
+++ config/rules.mk
@@ -0,0 +1,19 @@
+#BOOTMAGIC_ENABLE = lite
+
+# enable media keys
+EXTRAKEY_ENABLE = yes
+
+# smaller file size, little down-side
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# for combos!
+COMBO_ENABLE = yes
+
+# markstos
+# Backlighting is not RGB underglow or RGB matrix
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+
+# RGB
+RGBLIGHT_ENABLE = no
+
+TAP_DANCE_ENABLE = yes
